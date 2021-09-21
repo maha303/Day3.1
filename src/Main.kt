33965bf1f632c0e85d0a9568f943f5b58668695e
maha303 <86849690+maha303@users.noreply.github.com>
2021-09-21 16:40:33 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 fun main(){
-    val days = mapOf(1 to "sun", 2 to "Mon", 3 to "Tue",4 to "Wed",5 to "Thu",6 to "Fri",7 to "Sat")
+    val days = mapOf<Int, String>(1 to "sun", 2 to "Mon", 3 to "Tue",4 to "Wed",5 to "Thu",6 to "Fri",7 to "Sat")
     for (d in days){
-        println(d)
+        println("${d.key}:${d.value}")
     }
 }
